c5cbcd9d618b5660fbcb3df4f0cc3889b01e181b
Aftab-Laptop <aftab.uddin.games@gmail.com>
2025-01-19 00:48:58 +0600

    Added Dot Net console practice project

---
+++ DotNetConsolePractice/DsaPractice/DsaPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DsaPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {9, 0, -4, 1, 7, 3, 6, 15, 2, 5 };
+            Console.WriteLine("Unsorted: " + string.Join(",", myArray));
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.Sort(myArray);
+            Console.WriteLine("Sorted: " + string.Join(",", myArray));
+        }
+    }
+}
