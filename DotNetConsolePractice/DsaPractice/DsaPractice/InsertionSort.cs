3330e81a931ca8ddc2f1f01a80cb2f9bb5bdaae3
Aftab-Laptop <aftab.uddin.games@gmail.com>
2025-01-25 21:10:57 +0600

    Insertion sort added in vs dot net project

---
+++ DotNetConsolePractice/DsaPractice/DsaPractice/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace DsaPractice
+{
+    internal class InsertionSort
+    {
+        public void Sort(int[] array)
+        {
+            int itemsCount = array.Length;
+            for (int i = 1; i < itemsCount; i++)
+            {
+                int currentNumber = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > currentNumber)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = currentNumber;
+            }
+        }
+    }
+}
--- DotNetConsolePractice/DsaPractice/DsaPractice/Program.cs
+++ DotNetConsolePractice/DsaPractice/DsaPractice/Program.cs
@@ -6,11 +6,31 @@ namespace DsaPractice
     {
         static void Main(string[] args)
         {
-            int[] myArray = {9, 0, -4, 1, 7, 3, 6, 15, 2, 5 };
-            Console.WriteLine("Unsorted: " + string.Join(",", myArray));
+            BubbleSort();
+            InsertionSort();
+        }
+
+        static void BubbleSort()
+        {
+            int[] myArray = { 9, 0, -4, 1, 7, 3, 6, 15, 2, 5 };
+            Console.WriteLine("Bubble Sort - Unsorted: " + string.Join(",", myArray));
             BubbleSort bubbleSort = new BubbleSort();
             bubbleSort.Sort(myArray);
             Console.WriteLine("Sorted: " + string.Join(",", myArray));
+        }
+
+        static void InsertionSort()
+        {
+            Random randomNumber = new Random();
+            int[] myArray = new int[10];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = randomNumber.Next(20);
+            }
+            Console.WriteLine("Insertion Sort - Unsorted Array: " + string.Join(", ", myArray));
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.Sort(myArray);
+            Console.WriteLine("Sorted Array: " + string.Join(", ", myArray));
         }
     }
 }
