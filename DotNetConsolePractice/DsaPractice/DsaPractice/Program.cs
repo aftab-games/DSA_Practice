d78e78d34cacd730cec53ed211af996bfa6d1395
Aftab-Laptop <aftab.uddin.games@gmail.com>
2025-01-25 21:35:31 +0600

    Merge sort added to DotNet project

--- DotNetConsolePractice/DsaPractice/DsaPractice/Program.cs
+++ DotNetConsolePractice/DsaPractice/DsaPractice/Program.cs
@@ -9,6 +9,7 @@ namespace DsaPractice
             BubbleSort();
             InsertionSort();
             QuickSort();
+            MergeSort();
         }
 
         static void BubbleSort()
@@ -45,6 +46,20 @@ namespace DsaPractice
             Console.WriteLine("Quick Sort - Unsorted Array: " + string.Join(", ", myArray));
             QuickSort quickSort = new QuickSort();
             quickSort.Sort(myArray);
+            Console.WriteLine("Sorted Array: " + string.Join(", ", myArray));
+        }
+
+        static void MergeSort()
+        {
+            Random randomNumber = new Random();
+            int[] myArray = new int[10]; // {9, 3, 5, 2, 6, 1, 8, 4, 14, 17, 3, 7, 3, 25};
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = randomNumber.Next(20);
+            }
+            Console.WriteLine("Merge Sort - Unsorted Array: " + string.Join(", ", myArray));
+            MergeSort mergeSort = new MergeSort();
+            mergeSort.MergeAndSort(myArray);
             Console.WriteLine("Sorted Array: " + string.Join(", ", myArray));
         }
     }
